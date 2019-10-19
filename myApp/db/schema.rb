00241830bef91a51f111382907e03ec9d26e6cf0
EC2 Default User <ec2-user@ip-172-31-84-246.ec2.internal>
2019-10-19 00:16:41 +0000

    user lists are filtered

--- myApp/db/schema.rb
+++ myApp/db/schema.rb
@@ -11,9 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
-    create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "permissionLevel"
-    t.string   "linkedInUrl"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "permissionLevel"
+    t.string "linkedInUrl"
   end
+
 end
