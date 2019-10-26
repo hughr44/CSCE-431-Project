0505f468778243b17943152b7cdb08cf8b2b9bee
EC2 Default User <ec2-user@ip-172-31-47-122.us-west-2.compute.internal>
2019-10-26 16:27:16 +0000

    added route to oauth

--- myApp/config/routes.rb
+++ myApp/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
     get 'user' => 'user#user'
+    get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
+    get ‘auth/failure’, to: redirect(‘/’)
     resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
