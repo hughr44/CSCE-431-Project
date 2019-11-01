68952f50b38c9c65f039147760b6cc984b6d2c01
jasonlien0312 <ec2-user@ip-172-31-28-7.us-east-2.compute.internal>
2019-11-01 18:50:23 +0000

    added edit event function2

--- myApp/app/controllers/event_controller.rb
+++ myApp/app/controllers/event_controller.rb
@@ -11,7 +11,7 @@ class EventController < ApplicationController
     end
     
     def create
-        #TODO eventID counter
+        #TODO idk why event.params doesn't work
         # if(session[:user].permissionLevel=='admin')
             Event.create!(:eventID => params['eventID'], :eventName => params['eventName'], :eventDescription => params['eventDescription'], :eventImage => params['eventImage'], :eventUsers => params['eventUsers'])
         # end    
@@ -24,7 +24,18 @@ class EventController < ApplicationController
     end
     
     def edit
-        #TODO
+        @event = Event.find(params[:id])
+    end
+    def update
+        @event = Event.find(params[:id])
+        #TODO idk why event.params doesn't work
+        @event.update_attributes!(event_params)
+        #@event.update_attributes!(:eventID => @event.id, :eventName => params['eventName'], :eventDescription => params['eventDescription'], :eventImage => params['eventImage'], :eventUsers => params['eventUsers'])
+        redirect_to event_path
+    end
+    def destroy
+        blah
     end
     #TODO add eventDate
+    #take out id
 end
