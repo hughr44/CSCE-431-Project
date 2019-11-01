69fdbeffae60af265210d4d734d4a612b4fef0c5
jasonlien0312 <ec2-user@ip-172-31-28-7.us-east-2.compute.internal>
2019-11-01 08:09:21 +0000

    implemented show page

--- myApp/app/controllers/event_controller.rb
+++ myApp/app/controllers/event_controller.rb
@@ -1,13 +1,30 @@
 class EventController < ApplicationController
-    def user_params
+    def event_params
         params.require(:event).permit(:eventID, :eventName, :eventDescription, :eventImage, :eventUsers)
+    end
+    
+    def new 
+       @event = Event.new 
     end
     def event 
         @event = Event.all
     end
     
     def create
-        Event.create!(:eventID => params['eventID'], :eventName => params['eventName'], :eventDescription => params['eventDescription'], :eventImage => params['eventImage'], :eventUsers => params['eventUsers'])
+        #TODO eventID counter
+        # if(session[:user].permissionLevel=='admin')
+            Event.create!(:eventID => params['eventID'], :eventName => params['eventName'], :eventDescription => params['eventDescription'], :eventImage => params['eventImage'], :eventUsers => params['eventUsers'])
+        # end    
         redirect_to event_path
     end
+    
+    def show
+        @event = Event.find(params[:id])
+        # redirect_to user
+    end
+    
+    def edit
+        #TODO
+    end
+    #TODO add eventDate
 end
--- myApp/config/routes.rb
+++ myApp/config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     
     get 'event' => 'event#event'
     resources :event
+    
+    get 'event.:id' => 'event#show'
+    resources :event
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
