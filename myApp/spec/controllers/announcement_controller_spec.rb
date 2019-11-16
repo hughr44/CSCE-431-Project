e35a3003bb841694cc5946164760e65977707aa0
hughr44 <39559723+hughr44@users.noreply.github.com>
2019-11-15 18:53:31 -0600

    Update announcement_controller_spec.rb

--- myApp/spec/controllers/announcement_controller_spec.rb
+++ myApp/spec/controllers/announcement_controller_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe AnnouncementController, type: :controller do
-
+    
+describe 'Testing AnnouncementController' do
+        
+        describe 'destroy' do
+            before do
+                params = {id: 1}
+                post :destroy, params: params
+            end
+            
+            it 'calling destroy on an existing announcement should be successful' do
+                expect(AnnouncementController.new.destroy).to be_successful
+            end
+        end
+        
+        describe 'update' do
+            before do
+                params = {id: 1}
+                post :update, params: params
+            end
+            
+            it 'calling update on an existing announcement should be successful' do
+                expect(AnnouncementController.new.update).to be_successful
+            end
+        end
+        
+        describe 'edit' do
+            before do
+                params = {id: 1}
+                post :edit, params: params
+            end
+            
+            it 'calling edit on an existing announcement should be successful' do
+                expect(AnnouncementController.new.edit).to be_successful
+            end
+        end
+        
+        describe 'create' do
+            before do
+                params = {id: 1}
+                post :create, params: params
+            end
+            
+            it 'calling create on an should be successful' do
+                expect(AnnouncementController.new.create).to be_successful
+            end
+        end
+        
+        describe 'show' do
+            it 'calling show should be successful' do
+                expect(AnnouncementController.new.show).to be_successful
+            end
+        end
+        
+        describe 'officer_show' do
+            it 'calling officer_show should be successful' do
+                expect(AnnouncementController.new.officer_show).to be_successful
+            end
+        end
+        
+    end
 end
