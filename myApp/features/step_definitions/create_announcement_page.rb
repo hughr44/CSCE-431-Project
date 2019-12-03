87a6df68aa08360792dd1758590e12d864e93a68
bfpinkerton <43506110+bfpinkerton@users.noreply.github.com>
2019-12-03 13:51:06 -0600

    Delete create_announcement_page.rb

--- myApp/features/step_definitions/create_announcement_page.rb
+++
@@ -1,6 +0,0 @@
-When(/^member goes to announcements page$/) do
-  visit "https://tranquil-atoll-38693.herokuapp.com/announcement"
-end 
-Then(/^member can see announcements$/) do
-  expect(page).to have_content("Annoucements")
-end 
