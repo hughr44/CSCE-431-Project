1258dd6a6f3b58adc2b97fea0f118731f5613b79
hughr44 <39559723+hughr44@users.noreply.github.com>
2019-12-03 14:03:05 -0600

    Update announcement_controller_spec.rb

--- myApp/spec/controllers/announcement_controller_spec.rb
+++ myApp/spec/controllers/announcement_controller_spec.rb
@@ -5,13 +5,22 @@ RSpec.describe AnnouncementController, type: :controller do
 describe 'Testing AnnouncementController' do
         
         describe 'destroy' do
-            before do
+            it 'first testing proper announcement creation before calling destroy' do
+                @testAnn = Announcement.create(announcementTitle: "test anouncement", announcementText: "test test")
+                @testAnn = Announcement.find_by(announcementTitle: "test anouncement")
+                expect(Announcement.find_by(announcementTitle: "test anouncement")).to eq(@testAnn)
+            end
+        end
+        
+        describe 'destroy' do
+            it 'calling destroy on an existing announcement should be successful' do
+                @testAnn = Announcement.create(announcementTitle: "test anouncement", announcementText: "test test")
+                @testAnn = Announcement.find_by(announcementTitle: "test anouncement")
+                
                 params = {id: 1}
                 post :destroy, params: params
-            end
-            
-            it 'calling destroy on an existing announcement should be successful' do
-                expect(AnnouncementController.new.destroy).to be_successful
+                
+                expect(Announcement.find_by(announcementTitle: "test anouncement")).to eq(nil)
             end
         end
         
@@ -19,44 +28,54 @@ describe 'Testing AnnouncementController' do
             before do
                 params = {id: 1}
                 post :update, params: params
+                Announcement.create!(:announcementTitle => "test anouncement", :announcementText => "test test")
             end
             
             it 'calling update on an existing announcement should be successful' do
+                @testAnn = Announcement.create(:announcementTitle => "test anouncement", :announcementText => "test test")
+                
+                params = {id: 1}
+                post :update, params: params
+                
                 expect(AnnouncementController.new.update).to be_successful
             end
         end
         
         describe 'edit' do
-            before do
+            it 'calling edit on an existing announcement should be successful' do
+                @testAnn = Announcement.create(:announcementTitle => "test anouncement", :announcementText => "test test")
+                
                 params = {id: 1}
                 post :edit, params: params
-            end
-            
-            it 'calling edit on an existing announcement should be successful' do
-                expect(AnnouncementController.new.edit).to be_successful
+                
+                expect(response).to have_http_status(:success)
             end
         end
         
         describe 'create' do
-            before do
-                params = {id: 1}
-                post :create, params: params
-            end
-            
             it 'calling create on an should be successful' do
-                expect(AnnouncementController.new.create).to be_successful
+                params = {announcementTitle: "test anouncement", announcementText: "test test"}
+                post :create, params: params
+                
+                @testAnn = Announcement.find_by(announcementTitle: "test anouncement")
+                
+                expect(Announcement.find_by(announcementTitle: "test anouncement")).to eq(@testAnn)
             end
         end
         
         describe 'show' do
             it 'calling show should be successful' do
-                expect(AnnouncementController.new.show).to be_successful
+                post :show
+                
+                expect(response).to have_http_status(:success)
             end
         end
         
         describe 'officer_show' do
             it 'calling officer_show should be successful' do
-                expect(AnnouncementController.new.officer_show).to be_successful
+                post :officer_show
+                
+                expect(response).to have_http_status(:success)
             end
         end
         
