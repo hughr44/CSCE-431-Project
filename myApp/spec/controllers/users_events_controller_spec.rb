3de319a5ca5665b00e105e04c626275bd21ecfeb
hughr44 <39559723+hughr44@users.noreply.github.com>
2019-12-03 15:03:31 -0600

    Update users_events_controller_spec.rb

--- myApp/spec/controllers/users_events_controller_spec.rb
+++ myApp/spec/controllers/users_events_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe UsersEventsController, type: :controller do
-
+    
+describe 'Testing UsersEventsController' do
+        
+        describe 'create' do
+            it 'create (register) for an event should be successful' do
+                params = {email: 'fake email', eventName: 'fake event'}
+                post :create, params: params
+                
+                @testEvent = UsersEvent.find_by(eventName: 'fake event')
+      
+                expect(UsersEvent.find_by(eventName: 'fake event')).to eq(@testEvent)
+            end
+        end
+        
+        describe 'destroy' do
+            it 'calling destroy on an existing event should be successful' do
+                params = {email: 'fake email', eventName: 'fake event'}
+                post :create, params: params
+                
+                params = {id: 1}
+                post :destroy, params: params
+      
+                expect(UsersEvent.find_by(eventName: 'fake event')).to eq(nil)
+            end
+        end
+        
+    end
 end
