5c44d916881c52fed63e7d9eef8f4ae1f3e4ef8f
bfpinkerton <43506110+bfpinkerton@users.noreply.github.com>
2019-12-03 13:51:20 -0600

    Delete delete_annoucements.rb

--- myApp/features/step_definitions/delete_annoucements.rb
+++
@@ -1,9 +0,0 @@
-When(/^officer goes to announcements page$/) do
-  visit "https://tranquil-atoll-38693.herokuapp.com/officer/announcement"
-end 
-Then(/^officer clicks delete$/) do
-  find_button('delete').click
-end 
-Then(/^announcement is deleted$/) do
-    expect(page).not_to have_content("Dell")
-end
